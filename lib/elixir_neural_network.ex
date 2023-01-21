5e015a4fab47fd32e3c48df982cd0aef24d76ba5
Rishab Garg <rishgarg@gmail.com>
2023-01-21 12:43:47 +0100

    init: initialised elixir base project

---
+++ lib/elixir_neural_network.ex
@@ -0,0 +1,18 @@
+defmodule ElixirNeuralNetwork do
+  @moduledoc """
+  Documentation for `ElixirNeuralNetwork`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirNeuralNetwork.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_neural_network_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirNeuralNetworkTest do
+  use ExUnit.Case
+  doctest ElixirNeuralNetwork
+
+  test "greets the world" do
+    assert ElixirNeuralNetwork.hello() == :world
+  end
+end
